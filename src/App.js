6be8336696f1cce9250b86146b37ecbda656e84f
Erez <erezmirav@gmail.com>
2023-01-10 17:12:51 +0200

    search tool created + updated css file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
+import { useState } from 'react';
+
 function App() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (search === '') return;
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=15&srsearch=${search}`;
+
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>My Wiki</h1>
-        <form className="search-box">
-          <input type="search" placeholder="Search.." />
+        <form className="search-box" onSubmit={handleSearch}>
+          <input
+            type="search"
+            placeholder="Search.."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </form>
-        <p className="search-results">Search Results: </p>
+        {searchInfo.totalhits ? (
+          <p className="search-results">
+            Search Results: {searchInfo.totalhits}{' '}
+          </p>
+        ) : (
+          ''
+        )}
       </header>
       <div className="results">
-        <div className="result">
-          <h3>Title</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, exercitationem.</p>
-          <a href="#">Read more</a>
-        </div>
+        {results.map((result, i) => {
+          const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+          return (
+            <div className="result" key={i}>
+              <h3>{result.title}</h3>
+              <p dangerouslySetInnerHTML={{ __html: result.snippet + '...'}}></p>
+              <a href={url} target="_blank" rel="noreferrer">
+                Read more
+              </a>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
