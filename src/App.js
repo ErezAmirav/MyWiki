6c71229c4686f779811f5a0c79f7a04fcb5da125
Erez <erezmirav@gmail.com>
2023-02-13 19:06:20 +0200

    fixed routers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Account from './pages/Account'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Account from './pages/Account';
 
 function App() {
   return (
-    <Router basename='/'>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+    <Router>
+      <HashRouter basename="/">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </HashRouter>
     </Router>
   );
 }
